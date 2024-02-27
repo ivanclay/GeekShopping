6ff842201111267abf4043817f99df35e01ef1de
Ivan Clay <ivan.clay.moura@gmail.com>
2024-02-27 17:03:06 -0300

    feat: created product entity

--- GeekShopping.ProductAPI/Model/Context/MySqlContext.cs
+++ GeekShopping.ProductAPI/Model/Context/MySqlContext.cs
@@ -6,4 +6,6 @@ public class MySqlContext : DbContext
 {
     public MySqlContext() { }
     public MySqlContext(DbContextOptions <MySqlContext>options): base(options) { }
+
+    public DbSet<Product> Products { get; set; }
 }
