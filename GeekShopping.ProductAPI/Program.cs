5ef4e4765fb55340caa79a8ca1aa77c85e5bf307
Ivan Clay <ivan.clay.moura@gmail.com>
2024-02-27 17:19:17 -0300

    feat: created and executed product migration

--- GeekShopping.ProductAPI/Program.cs
+++ GeekShopping.ProductAPI/Program.cs
@@ -1,5 +1,6 @@
 using GeekShopping.ProductAPI.Model.Context;
 using Microsoft.EntityFrameworkCore;
+using Microsoft.OpenApi.Models;
 
 var builder = WebApplication.CreateBuilder(args);
 
@@ -11,7 +12,10 @@ builder.Services.AddDbContext<MySqlContext>
 // Add services to the container.
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
-builder.Services.AddSwaggerGen();
+builder.Services.AddSwaggerGen(c =>
+{
+    c.SwaggerDoc("v1", new OpenApiInfo { Title = "GeekShopping.ProductAPI", Version = "v1"});
+});
 
 var app = builder.Build();
 
