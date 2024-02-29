f68519d4e114e593e96e819c5ef16cb3f4f7b9a6
Ivan Clay <ivan.clay.moura@gmail.com>
2024-02-29 08:38:24 -0300

    feat: added and configured product controller

---
+++ GeekShopping.ProductAPI/Controllers/ProductController.cs
@@ -0,0 +1,32 @@
+﻿using GeekShopping.ProductAPI.Data.ValueObjects;
+using GeekShopping.ProductAPI.Repository;
+using Microsoft.AspNetCore.Mvc;
+
+namespace GeekShopping.ProductAPI.Controllers;
+
+[Route("api/v1/[controller]")]
+public class ProductController : Controller
+{
+    private IProductRepository _productRepository;
+    public ProductController(IProductRepository repository)
+    {
+        _productRepository = repository ?? throw new ArgumentNullException(nameof(repository));
+    }
+
+    [HttpGet("{id}")]
+    public async Task<ActionResult<ProductVO>> FindById(long id)
+    {
+        var product = await _productRepository.FindById(id);
+        if (product == null) return NotFound();
+
+        return Ok(product);
+    }
+
+    [HttpGet]
+    public async Task<ActionResult<IEnumerable<ProductVO>>> FindAll()
+    {
+        var products = await _productRepository.FindAll();
+        if (products == null) return NotFound();
+        return Ok(products);
+    }
+}
--- GeekShopping.ProductAPI/Program.cs
+++ GeekShopping.ProductAPI/Program.cs
@@ -17,6 +17,8 @@ builder.Services.AddSingleton(mapper);
 builder.Services.AddAutoMapper(AppDomain.CurrentDomain.GetAssemblies());
 builder.Services.AddScoped<IProductRepository, ProductRepository>();
 
+builder.Services.AddControllers();
+
 // Add services to the container.
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
@@ -34,28 +36,19 @@ if (app.Environment.IsDevelopment())
     app.UseSwaggerUI();
 }
 
-var summaries = new[]
-{
-    "Freezing", "Bracing", "Chilly", "Cool", "Mild", "Warm", "Balmy", "Hot", "Sweltering", "Scorching"
-};
-
-app.MapGet("/weatherforecast", () =>
-{
-    var forecast = Enumerable.Range(1, 5).Select(index =>
-        new WeatherForecast
-        (
-            DateTime.Now.AddDays(index),
-            Random.Shared.Next(-20, 55),
-            summaries[Random.Shared.Next(summaries.Length)]
-        ))
-        .ToArray();
-    return forecast;
-})
-.WithName("GetWeatherForecast");
+app.MapControllers();
+//app.MapGet("/weatherforecast", () =>
+//{
+//    var forecast = Enumerable.Range(1, 5).Select(index =>
+//        new WeatherForecast
+//        (
+//            DateTime.Now.AddDays(index),
+//            Random.Shared.Next(-20, 55),
+//            summaries[Random.Shared.Next(summaries.Length)]
+//        ))
+//        .ToArray();
+//    return forecast;
+//})
+//.WithName("GetWeatherForecast");
 
 app.Run();
-
-internal record WeatherForecast(DateTime Date, int TemperatureC, string? Summary)
-{
-    public int TemperatureF => 32 + (int)(TemperatureC / 0.5556);
-}
