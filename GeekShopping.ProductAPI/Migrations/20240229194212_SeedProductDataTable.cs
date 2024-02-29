d1b26c969be6268b51d0e3a49a2edad1aa6a9d59
Ivan Clay <ivan.clay.moura@gmail.com>
2024-02-29 16:44:07 -0300

    feat: added seed migrations

---
+++ GeekShopping.ProductAPI/Migrations/20240229194212_SeedProductDataTable.cs
@@ -0,0 +1,106 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace GeekShopping.ProductAPI.Migrations
+{
+    public partial class SeedProductDataTable : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.InsertData(
+                table: "product",
+                columns: new[] { "id", "category_name", "description", "image_url", "name", "price" },
+                values: new object[,]
+                {
+                    { 1L, "Cup", "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Ut et massa non erat fringilla accumsan.", "https://github.com/ivanclay/GeekShopping/blob/main/images/1_super_mario.jpg", "Super Mario Cup", 39.9m },
+                    { 2L, "T-Shirt", "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Ut et massa non erat fringilla accumsan.", "https://github.com/ivanclay/GeekShopping/blob/main/images/2_no_internet.jpg", "T-Shirt No Internet", 49.9m },
+                    { 3L, "Lamp", "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Ut et massa non erat fringilla accumsan.", "https://github.com/ivanclay/GeekShopping/blob/main/images/3_vader.jpg", "Vader Lamp", 139.9m },
+                    { 4L, "Collection", "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Ut et massa non erat fringilla accumsan.", "https://github.com/ivanclay/GeekShopping/blob/main/images/4_storm_tropper.jpg", "Storm Tropper", 339.9m },
+                    { 5L, "T-Shirt", "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Ut et massa non erat fringilla accumsan.", "https://github.com/ivanclay/GeekShopping/blob/main/images/5_100_gamer.jpg", "100% Gamer", 39.9m },
+                    { 6L, "T-Shirt", "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Ut et massa non erat fringilla accumsan.", "https://github.com/ivanclay/GeekShopping/blob/main/images/6_spacex.jpg", "SpaceX", 39.9m },
+                    { 7L, "T-Shirt", "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Ut et massa non erat fringilla accumsan.", "https://github.com/ivanclay/GeekShopping/blob/main/images/7_coffee.jpg", "Coffee", 39.9m },
+                    { 8L, "T-Shirt", "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Ut et massa non erat fringilla accumsan.", "https://github.com/ivanclay/GeekShopping/blob/main/images/8_moletom_cobra_kay.jpg", "Cobra Kay", 99.9m },
+                    { 9L, "Book", "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Ut et massa non erat fringilla accumsan.", "https://github.com/ivanclay/GeekShopping/blob/main/images/9_neil.jpg", "Star Talk", 59.9m },
+                    { 10L, "Play", "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Ut et massa non erat fringilla accumsan.", "https://github.com/ivanclay/GeekShopping/blob/main/images/10_milennium_falcon.jpg", "Millenium Falcon", 539.9m },
+                    { 11L, "T-Shirt", "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Ut et massa non erat fringilla accumsan.", "https://github.com/ivanclay/GeekShopping/blob/main/images/11_mars.jpg", "Mars", 39.9m },
+                    { 12L, "T-Shirt", "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Ut et massa non erat fringilla accumsan.", "https://github.com/ivanclay/GeekShopping/blob/main/images/12_gnu_linux.jpg", "GNU LINUX", 39.9m },
+                    { 13L, "T-Shirt", "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Ut et massa non erat fringilla accumsan.", "https://github.com/ivanclay/GeekShopping/blob/main/images/13_dragon_ball.jpg", "Dragon Ball", 39.9m },
+                    { 14L, "Patch", "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Ut et massa non erat fringilla accumsan.", "https://github.com/ivanclay/GeekShopping/blob/main/images/14_patch_nasa.jpg", "NASA", 19.9m }
+                });
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DeleteData(
+                table: "product",
+                keyColumn: "id",
+                keyValue: 1L);
+
+            migrationBuilder.DeleteData(
+                table: "product",
+                keyColumn: "id",
+                keyValue: 2L);
+
+            migrationBuilder.DeleteData(
+                table: "product",
+                keyColumn: "id",
+                keyValue: 3L);
+
+            migrationBuilder.DeleteData(
+                table: "product",
+                keyColumn: "id",
+                keyValue: 4L);
+
+            migrationBuilder.DeleteData(
+                table: "product",
+                keyColumn: "id",
+                keyValue: 5L);
+
+            migrationBuilder.DeleteData(
+                table: "product",
+                keyColumn: "id",
+                keyValue: 6L);
+
+            migrationBuilder.DeleteData(
+                table: "product",
+                keyColumn: "id",
+                keyValue: 7L);
+
+            migrationBuilder.DeleteData(
+                table: "product",
+                keyColumn: "id",
+                keyValue: 8L);
+
+            migrationBuilder.DeleteData(
+                table: "product",
+                keyColumn: "id",
+                keyValue: 9L);
+
+            migrationBuilder.DeleteData(
+                table: "product",
+                keyColumn: "id",
+                keyValue: 10L);
+
+            migrationBuilder.DeleteData(
+                table: "product",
+                keyColumn: "id",
+                keyValue: 11L);
+
+            migrationBuilder.DeleteData(
+                table: "product",
+                keyColumn: "id",
+                keyValue: 12L);
+
+            migrationBuilder.DeleteData(
+                table: "product",
+                keyColumn: "id",
+                keyValue: 13L);
+
+            migrationBuilder.DeleteData(
+                table: "product",
+                keyColumn: "id",
+                keyValue: 14L);
+        }
+    }
+}
--- GeekShopping.ProductAPI/Model/Context/MySqlContext.cs
+++ GeekShopping.ProductAPI/Model/Context/MySqlContext.cs
@@ -1,4 +1,5 @@
 ﻿using Microsoft.EntityFrameworkCore;
+using static System.Net.WebRequestMethods;
 
 namespace GeekShopping.ProductAPI.Model.Context;
 
@@ -6,6 +7,164 @@ public class MySqlContext : DbContext
 {
     public MySqlContext() { }
     public MySqlContext(DbContextOptions <MySqlContext>options): base(options) { }
-
     public DbSet<Product> Products { get; set; }
+    protected override void OnModelCreating(ModelBuilder modelBuilder)
+    {
+        const string IMG_URL_BASE = "https://github.com/ivanclay/GeekShopping/blob/main/images/";
+
+        modelBuilder.Entity<Product>().HasData(
+            new Product 
+            {
+                Id = 1,
+                Name = "Super Mario Cup",
+                Price = new decimal(39.90),
+                CategoryName = "Cup",
+                Description = "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Ut et massa non erat fringilla accumsan.",
+                ImageUrl = $"{IMG_URL_BASE}1_super_mario.jpg"
+            }
+        );
+        modelBuilder.Entity<Product>().HasData(
+            new Product
+            {
+                Id = 2,
+                Name = "T-Shirt No Internet",
+                Price = new decimal(49.90),
+                CategoryName = "T-Shirt",
+                Description = "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Ut et massa non erat fringilla accumsan.",
+                ImageUrl = $"{IMG_URL_BASE}2_no_internet.jpg"
+            }
+        );
+        modelBuilder.Entity<Product>().HasData(
+            new Product
+            {
+                Id = 3,
+                Name = "Vader Lamp",
+                Price = new decimal(139.90),
+                CategoryName = "Lamp",
+                Description = "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Ut et massa non erat fringilla accumsan.",
+                ImageUrl = $"{IMG_URL_BASE}3_vader.jpg"
+            }
+        );
+        modelBuilder.Entity<Product>().HasData(
+            new Product
+            {
+                Id = 4,
+                Name = "Storm Tropper",
+                Price = new decimal(339.90),
+                CategoryName = "Collection",
+                Description = "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Ut et massa non erat fringilla accumsan.",
+                ImageUrl = $"{IMG_URL_BASE}4_storm_tropper.jpg"
+            }
+        );
+        modelBuilder.Entity<Product>().HasData(
+            new Product
+            {
+                Id = 5,
+                Name = "100% Gamer",
+                Price = new decimal(39.90),
+                CategoryName = "T-Shirt",
+                Description = "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Ut et massa non erat fringilla accumsan.",
+                ImageUrl = $"{IMG_URL_BASE}5_100_gamer.jpg"
+            }
+        );
+        modelBuilder.Entity<Product>().HasData(
+            new Product
+            {
+                Id = 6,
+                Name = "SpaceX",
+                Price = new decimal(39.90),
+                CategoryName = "T-Shirt",
+                Description = "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Ut et massa non erat fringilla accumsan.",
+                ImageUrl = $"{IMG_URL_BASE}6_spacex.jpg"
+            }
+        );
+        modelBuilder.Entity<Product>().HasData(
+            new Product
+            {
+                Id = 7,
+                Name = "Coffee",
+                Price = new decimal(39.90),
+                CategoryName = "T-Shirt",
+                Description = "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Ut et massa non erat fringilla accumsan.",
+                ImageUrl = $"{IMG_URL_BASE}7_coffee.jpg"
+            }
+        );
+        modelBuilder.Entity<Product>().HasData(
+            new Product
+            {
+                Id = 8,
+                Name = "Cobra Kay",
+                Price = new decimal(99.90),
+                CategoryName = "T-Shirt",
+                Description = "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Ut et massa non erat fringilla accumsan.",
+                ImageUrl = $"{IMG_URL_BASE}8_moletom_cobra_kay.jpg"
+            }
+        );
+        modelBuilder.Entity<Product>().HasData(
+            new Product
+            {
+                Id = 9,
+                Name = "Star Talk",
+                Price = new decimal(59.90),
+                CategoryName = "Book",
+                Description = "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Ut et massa non erat fringilla accumsan.",
+                ImageUrl = $"{IMG_URL_BASE}9_neil.jpg"
+            }
+        );
+        modelBuilder.Entity<Product>().HasData(
+            new Product
+            {
+                Id = 10,
+                Name = "Millenium Falcon",
+                Price = new decimal(539.90),
+                CategoryName = "Play",
+                Description = "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Ut et massa non erat fringilla accumsan.",
+                ImageUrl = $"{IMG_URL_BASE}10_milennium_falcon.jpg"
+            }
+        );
+        modelBuilder.Entity<Product>().HasData(
+            new Product
+            {
+                Id = 11,
+                Name = "Mars",
+                Price = new decimal(39.90),
+                CategoryName = "T-Shirt",
+                Description = "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Ut et massa non erat fringilla accumsan.",
+                ImageUrl = $"{IMG_URL_BASE}11_mars.jpg"
+            }
+        );
+        modelBuilder.Entity<Product>().HasData(
+            new Product
+            {
+                Id = 12,
+                Name = "GNU LINUX",
+                Price = new decimal(39.90),
+                CategoryName = "T-Shirt",
+                Description = "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Ut et massa non erat fringilla accumsan.",
+                ImageUrl = $"{IMG_URL_BASE}12_gnu_linux.jpg"
+            }
+        );
+        modelBuilder.Entity<Product>().HasData(
+            new Product
+            {
+                Id = 13,
+                Name = "Dragon Ball",
+                Price = new decimal(39.90),
+                CategoryName = "T-Shirt",
+                Description = "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Ut et massa non erat fringilla accumsan.",
+                ImageUrl = $"{IMG_URL_BASE}13_dragon_ball.jpg"
+            }
+        );
+        modelBuilder.Entity<Product>().HasData(
+            new Product
+            {
+                Id = 14,
+                Name = "NASA",
+                Price = new decimal(19.90),
+                CategoryName = "Patch",
+                Description = "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Ut et massa non erat fringilla accumsan.",
+                ImageUrl = $"{IMG_URL_BASE}14_patch_nasa.jpg"
+            }
+        );
+    }
 }
