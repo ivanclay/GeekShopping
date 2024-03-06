0831ede2958e87aff01b0b5a4b538e520bfbbd0c
Ivan Clay <ivan.clay.moura@gmail.com>
2024-03-06 10:37:53 -0300

    feat: created product api service contract

---
+++ GeekShopping.Web/Services/IServices/IProductService.cs
@@ -0,0 +1,12 @@
+﻿using GeekShopping.Web.Models;
+
+namespace GeekShopping.Web.Services.IServices;
+
+public interface IProductService
+{
+    Task<IEnumerable<ProductModel>> FindAllProducts();
+    Task<ProductModel> FindById(long id);
+    Task<ProductModel> CreateProduct(ProductModel model);
+    Task<ProductModel> UpdateProduct(ProductModel model);
+    Task<bool> DeleteProductById(long id);
+}
