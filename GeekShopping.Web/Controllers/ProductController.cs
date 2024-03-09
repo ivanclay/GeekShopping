f765104a2ca6a3346aafffa0fd23aaa4af307162
Ivan Clay <ivan.clay.moura@gmail.com>
2024-03-08 21:17:29 -0300

    feat: added product create methods

--- GeekShopping.Web/Controllers/ProductController.cs
+++ GeekShopping.Web/Controllers/ProductController.cs
@@ -1,4 +1,5 @@
-﻿using GeekShopping.Web.Services.IServices;
+﻿using GeekShopping.Web.Models;
+using GeekShopping.Web.Services.IServices;
 using Microsoft.AspNetCore.Mvc;
 
 namespace GeekShopping.Web.Controllers;
@@ -16,5 +17,22 @@ public class ProductController : Controller
     {
         var products = await _productService.FindAllProducts();
         return View(products);
+    }
+
+    public async Task<IActionResult> ProductCreate()
+    {
+        return View();
+    }
+
+    [HttpPost]
+    public async Task<IActionResult> ProductCreate(ProductModel model)
+    {
+        if (ModelState.IsValid) 
+        {
+            var response = await _productService.CreateProduct(model);
+            if (response != null) return RedirectToAction(nameof(ProductIndex));
+        }
+
+        return View(model);
     }
 }
